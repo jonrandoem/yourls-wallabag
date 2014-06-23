9643ec05ff170c111cac25adad30e1daf6fcf0f6
jonrandoem <contact@jonrandoem.com>
2014-06-23 17:14:35 +0200

    Update Poche to Wallabag

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-yourls-poche
+yourls-wallabag
 ============
 
 About
 ------------
 
-yourls-poche is a plugin for [YOURLS](http://yourls.org/). The plugin adds a new share link into the Quick Share box (available for each URL shown in the Yourls admin area).
-The added link allows the sharing of the URL to a [Poche](http://http://www.inthepoche.com/en/) installation.
+yourls-wallabag is a plugin for [YOURLS](http://yourls.org/). The plugin adds a new share link into the Quick Share box (available for each URL shown in the Yourls admin area).
+The added link allows the sharing of the URL to a [Wallabag (previously named Poche)](https://www.wallabag.org/) installation.
 
-Obviously, you need a valid Yourls installation and a valid Poche installation.
+Obviously, you need a valid Yourls installation and a valid Wallabag installation.
 
 This plugin has been tested on Yourls:
 * [1.6](https://github.com/YOURLS/YOURLS/releases/tag/1.6)
@@ -17,13 +17,13 @@ This plugin has been tested on Yourls:
 Installation
 ------------
 
-1. Unzip the plugin. You get a "yourls-poche" folder
+1. Unzip the plugin. You get a "yourls-wallabag" folder
 2. Upload this folder into the user/plugins folder of your Yourls installation
 3. Edit your config.php file (located at user/config.php in your Yourls installation)
-4. Define the URL of your Poche installation by inserting the following line at the end of your config.php file. Adapt the URL "https://my-poche.com" to your Poche installation's URL (No trailing slash !!!), and save changes to config.php
+4. Define the URL of your Poche installation by inserting the following line at the end of your config.php file. Adapt the URL "https://my-wallabag.com" to your Wallabag installation's URL (No trailing slash !!!), and save changes to config.php
 
 ```
-define('POCHE_URL', 'https://my-poche.com');
+define('WALLABAG_URL', 'https://my-wallabag.com');
 ```
 
 Finally, activate the plugin in the admin area
--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
-Plugin Name: Share to Poche
-Plugin URI: https://github.com/jonrandoem/yourls-poche
-Description: Add <a href="http://http://www.inthepoche.com/en/">Poche</a> to the list of Quick Share destinations
+Plugin Name: Share to Wallabag
+Plugin URI: https://github.com/jonrandoem/yourls-wallabag
+Description: Add <a href="https://www.wallabag.org/">Wallabag</a> to the list of Quick Share destinations
 Version: 1.0
 Author: JonRandoem
 Author URI: https://github.com/jonrandoem/
@@ -11,30 +11,30 @@ Author URI: https://github.com/jonrandoem/
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-yourls_add_action( 'share_links', 'jr_yourls_poche' );
+yourls_add_action( 'share_links', 'jr_yourls_wallabag' );
 
-function jr_yourls_poche( $args ) {
+function jr_yourls_wallabag( $args ) {
 	list( $longurl, $shorturl, $title, $text ) = $args;
 	$longurl = rawurlencode( $longurl );
-	if ( defined('POCHE_URL') ) {
-		$poche_url = POCHE_URL;
-		echo <<<POCHE
+	if ( defined('WALLABAG_URL') ) {
+		$poche_url = WALLABAG_URL;
+		echo <<<WALLABAG
 		
 		<style type="text/css">
-		#share_poche{background:url('../user/plugins/yourls-poche/poche.png') left center no-repeat;}
+		#share_wallabag{background:url('../user/plugins/yourls-wallabag/wallabag.png') left center no-repeat;}
 		</style>
 		
 		<a id="share_poche"
 			href="$poche_url/index.php?action=add&amp;plainurl=$longurl&amp;autoclose=true"
-			title="Share to Poche"
-			onclick="yourls_share_on_poche();return false;">Poche
+			title="Share to Wallabag"
+			onclick="yourls_share_on_wallabag();return false;">Wallabag
 		</a>
 		
 		<script type="text/javascript">
-		// Send to Poche open window
-		function yourls_share_on_poche() {
-			var url = $('#share_poche').attr('href');
-			window.open(url, 'poche', 'toolbar=no,width=800,height=600');
+		// Send to Wallabag open window
+		function yourls_share_on_wallabag() {
+			var url = $('#share_wallabag').attr('href');
+			window.open(url, 'wallabag', 'toolbar=no,width=800,height=600');
 			return false;
 		}
 		// Dynamically update Poche link
@@ -42,11 +42,11 @@ function jr_yourls_poche( $args ) {
 		$('#tweet_body').keypress(function(){
 			var title = encodeURIComponent( $('#titlelink').val() );
 			var url = encodeURIComponent( $('#copylink').val() );
-			var poche = '$poche_url/index.php?action=add&plainurl='+url+'&autoclose=true';
-			$('#share_poche').attr('href', poche);		
+			var wallabag = '$poche_url/index.php?action=add&plainurl='+url+'&autoclose=true';
+			$('#share_wallabag').attr('href', wallabag);		
 		});
 		</script>
 
-POCHE;
+WALLABAG;
 	}
 }
